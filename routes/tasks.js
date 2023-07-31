889af9d82b3c9059b2644aed522b1fb92ca27398
Lavie-3107 <nguyenanhtu2204tb@gmail.com>
2023-07-31 18:03:54 +0700

    update

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,7 +9,7 @@ router.get('/tasks/:userId', async (req, res) => {
     const tasks = await Task.find({ userId }); // Tìm các công việc của user dựa vào id user
     res.send(tasks);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).send("Láy danh sách công việc thất bại");
   }
 });
 
@@ -19,7 +19,9 @@ router.post('/tasks/:userId', async (req, res) => {
   const task = new Task({
     title: req.body.title,
     description: req.body.description,
-    userId
+    userId,
+    status:req.body.status,
+    time:req.body.time
   });
 
   try {
@@ -32,9 +34,9 @@ router.post('/tasks/:userId', async (req, res) => {
     // Lưu lại mảng tasks đã được cập nhật
     const updatedTasks = await Promise.all(tasksWithUserId.map(task => task.save()));
 
-    res.status(201).json(updatedTasks); // Trả về danh sách các task sau khi thêm mới
+    res.status(201).send("Thêm công việc thành công"); // Trả về danh sách các task sau khi thêm mới
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).send("Thêm công việc thất bại");
   }
 });
 
@@ -43,35 +45,49 @@ router.post('/tasks/:userId', async (req, res) => {
 //   res.json(res.task); 
 // });
 
-// // API cập nhật thông tin công việc theo ID
-// router.put('/tasks/:id', getTask, async (req, res) => {
-//   if (req.body.title != null) {
-//     res.task.title = req.body.title;
-//   }
+// API cập nhật thông tin công việc theo ID
+router.put('/tasks/:id', async (req, res) => {
+  const taskId = req.params.id;
 
-//   if (req.body.description != null) {
-//     res.task.description = req.body.description;
-//   }
+  try {
+    // Find the task by ID
+    const task = await Task.findById(taskId);
 
-//   try {
-//     const updatedTask = await res.task.save();
-//     res.json(updatedTask);
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// });
+    if (!task) {
+      // Task with the provided ID not found
+      return res.status(404).send('Công việc không tồn tại');
+    }
 
-// // API xóa công việc theo ID
-// router.delete('/tasks/:id', getTask, async (req, res) => {
-//   try {
-//     // await res.task.remove();
-//     await  Task.findOneAndDelete(res.task._id)
-//     res.json({ message: 'Xóa công việc thành công!' });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
+    // Update the task properties if they exist in the request body
+    task.title = req.body.title;
+    task.description = req.body.description;
+    task.time = req.body.time;
+    task.status = req.body.status;
+    // Save the updated task
+    const updatedTask = await task.save();
+    res.send("Cập nhật công việc thành công");
+  } catch (error) {
+    res.status(400).send("Cập nhật công việc không thành công");
+  }
+});
 
-// Middleware để lấy thông tin công việc theo ID
+// API xóa công việc theo ID
+router.delete('/tasks/:id', async (req, res) => {
+  try {
+    const taskId = req.params.id;
+
+    // Find the task by its ID and delete it
+    const deletedTask = await Task.findByIdAndDelete(taskId);
+
+    // Check if the task was found and deleted successfully
+    if (!deletedTask) {
+      return res.status(404).send('Công việc không tồn tại');
+    }
+
+    res.send('Xóa công việc thành công!' );
+  } catch (error) {
+    res.status(500).send('Xóa công việc thất bại');
+  }
+});
 
 module.exports = router;
